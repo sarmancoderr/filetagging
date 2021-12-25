bbac153f4400f7753e075bade417613e951eb03c
raul <sarmancoder@gmail.com>
2021-12-25 21:38:56 +0100

    Limpiar proyecto

--- src/renderer/layouts/default.vue
+++ src/renderer/layouts/default.vue
@@ -17,14 +17,7 @@
         <v-card-title primary-title>
           <h3 class="headline mb-0">Listado de etiquetas</h3>
         </v-card-title>
-        <v-card-text>
-          <v-chip
-            v-for="tag in tags" :key="tag" small class="mr-4"
-            :close="isTagSelected(tag.name)" @click="tagSelection(tag)" @click:close="removeTagSelection(tag)"
-          >
-            {{ tag.name }}
-          </v-chip>
-        </v-card-text>
+        <v-card-text />
       </v-card>
     </v-navigation-drawer>
 
@@ -37,21 +30,7 @@
 </template>
 
 <script>
-import { mapActions, mapGetters, mapState, mapMutations } from 'vuex'
-export default {
-    name: 'DefaultLayout',
-    computed: {
-        ...mapGetters('db', ['tags']),
-        ...mapState('db', ['tagsSelected'])
-    },
-    methods: {
-        ...mapMutations('db', ['tagSelection', 'removeTagSelection']),
-        ...mapActions('fs', ['upLevel']),
-        isTagSelected (name) {
-            return this.tagsSelected.filter(t => t.name === name)[0] !== undefined
-        }
-    }
-}
+export default {}
 </script>
 
 <style>
